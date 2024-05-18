3f77b63f0be0a1810ae2c4b3515de7cce080c2b3
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-18 12:20:38 -0300

    Initial Commit

---
+++ CountdownTimerApp/CountdownTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerAppApp.swift
+//  CountdownTimerApp
+//
+//  Created by Gabriel Lopes on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
